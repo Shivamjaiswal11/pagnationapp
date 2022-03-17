07eb152ecb67b051917cdb22381ec23c7f6b270a
shivamjaiswal1191996@gmail.com <shivamjaiswal1191996@gmail.com>
2022-03-17 17:14:01 +0530

    add service genrater

---
+++ app/src/main/java/com/example/pagenationapp/Network/ServiceGenerator.java
@@ -0,0 +1,26 @@
+package com.example.pagenationapp.Network;
+
+import okhttp3.OkHttpClient;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class ServiceGenerator {
+
+    public static final String API_BASE_URL = "http://your.api-base.url";
+
+    private static OkHttpClient httpClient;
+
+    static {
+        httpClient = new OkHttpClient();
+    }
+
+    private static Retrofit.Builder builder =
+            new Retrofit.Builder()
+                    .baseUrl(API_BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create());
+
+    public static <S> S createService(Class<S> serviceClass) {
+        Retrofit retrofit = builder.client(httpClient).build();
+        return retrofit.create(serviceClass);
+    }
+}
---
+++ app/src/main/java/com/example/pagenationapp/Requestinterface.java
@@ -0,0 +1,4 @@
+package com.example.pagenationapp;
+
+public interface Requestinterface {
+}
