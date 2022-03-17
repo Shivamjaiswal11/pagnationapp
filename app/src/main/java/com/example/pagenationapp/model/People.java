0b1efc0048582fb422c4799c313f6efad19a9b24
Aman <amanchaurasiya786@gmail.com>
2022-03-17 16:13:21 +0530

    model added

---
+++ app/src/main/java/com/example/pagenationapp/model/People.java
@@ -0,0 +1,34 @@
+
+package com.example.pagenationapp.model;
+
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+
+public class People {
+
+    @SerializedName("status")
+    @Expose
+    private String status;
+    @SerializedName("approved_on")
+    @Expose
+    private String approvedOn;
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getApprovedOn() {
+        return approvedOn;
+    }
+
+    public void setApprovedOn(String approvedOn) {
+        this.approvedOn = approvedOn;
+    }
+
+}
---
+++ app/src/main/java/com/example/pagenationapp/model/TopicSubmissions.java
@@ -0,0 +1,23 @@
+
+package com.example.pagenationapp.model;
+
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+
+public class TopicSubmissions {
+
+    @SerializedName("people")
+    @Expose
+    private People people;
+
+    public People getPeople() {
+        return people;
+    }
+
+    public void setPeople(People people) {
+        this.people = people;
+    }
+
+}
