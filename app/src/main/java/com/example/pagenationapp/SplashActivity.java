37ec958616d21f46eefc767de0048e1090e8b86b
shivamjaiswal1191996@gmail.com <shivamjaiswal1191996@gmail.com>
2022-03-20 11:35:40 +0530

    splash sreen

---
+++ app/src/main/java/com/example/pagenationapp/SplashActivity.java
@@ -0,0 +1,49 @@
+package com.example.pagenationapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.LayoutInflater;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+
+import com.example.pagenationapp.databinding.ActivitySplashBinding;
+
+public class SplashActivity extends AppCompatActivity {
+    private ActivitySplashBinding b;
+    private static int SPLASH_SCREEN_TIME_OUT = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        b = ActivitySplashBinding.inflate(getLayoutInflater());
+        setContentView(b.getRoot());
+        getSupportActionBar().hide();
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashActivity.this,
+                        MainActivity.class);
+
+
+                startActivity(i);
+
+
+                finish();
+
+            }
+        }, SPLASH_SCREEN_TIME_OUT);
+        Animation myanimation= AnimationUtils.loadAnimation(SplashActivity.this,R.anim.animation2);
+        b.tv1.startAnimation(myanimation);
+        Animation myanimation1= AnimationUtils.loadAnimation(SplashActivity.this,R.anim.animation1);
+        b.tv1.startAnimation(myanimation1);
+    }
+}
